9b9fc07db7912ac884318c1023ae79127cb36b2f
Sechaba Mofokeng <sechaba@onemoola.com>
2023-07-31 18:38:08 +0200

    add functional style strategy pattern

---
+++ features/src/main/kotlin/StrategyPattern.kt
@@ -0,0 +1,25 @@
+fun sumValues(values: List<Int>, strategy: (Int) -> Int): Int {
+    return values.sumOf { value -> strategy(value) }
+}
+
+fun sumValuesUsingPredicate(values: List<Int>, strategy: (Int) -> Boolean): Int {
+    // OR return values.filter(strategy).sum()
+    return values.filter { value -> strategy(value) }.sum()
+}
+
+
+fun main() {
+    val values = listOf(1, 2, 3, 4, 5)
+    val sum = sumValues(values) { it * 2 }
+    println(sum)
+
+    val sum2 = sumValuesUsingPredicate(values) { it % 2 == 0 }
+    println(sum2)
+
+    val sumIgnore = sumValuesUsingPredicate(values) { true }
+    println(sumIgnore)
+
+    val isOdd = { number: Int -> number % 2 != 0 }
+    val sumOdd = sumValuesUsingPredicate(values, isOdd)
+    println(sumOdd)
+}
